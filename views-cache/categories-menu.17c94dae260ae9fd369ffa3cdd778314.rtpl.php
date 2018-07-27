839fae9d7afb13d2a14026f183a06590edf26282
zidane steigleder <zidanesteigleder@gmail.com>
2018-07-27 11:59:37 -0300

    adicionado categorias

---
+++ views-cache/categories-menu.17c94dae260ae9fd369ffa3cdd778314.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/2">Atendimento</a></li><li><a href="/categories/1">Consulta</a></li><li><a href="/categories/4">Gatos fofos</a></li><li><a href="/categories/5">Prostitutas</a></li>
