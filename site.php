b375a05e53904891797e228e53befd9ca5b03d99
zidane steigleder <zidanesteigleder@gmail.com>
2018-07-30 16:57:18 -0300

    detalhes prodts

--- site.php
+++ site.php
@@ -36,4 +36,16 @@ $page->setTpl("category", [
 
 ]);
 });
+
+$app->get("/products/:desurl", function($desurl){
+
+	$product = new Product();
+	$product->getFromURL($desurl);
+	$page = new Page();
+	$page->setTpl("product-detail", [
+		'product'=>$product->getValues(),
+		'categories'=>$product->getCategories()
+			]);
+
+});
 ?>
