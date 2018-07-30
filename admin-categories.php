f14f31b32e121a20fad4d65967b47cf1f76b780c
zidane steigleder <zidanesteigleder@gmail.com>
2018-07-30 13:22:55 -0300

    categorias vs produtos

--- admin-categories.php
+++ admin-categories.php
@@ -3,6 +3,7 @@
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
 use \Hcode\Model\Category;
+use \Hcode\Model\Product;
 
 $app->get("/admin/categories", function(){
 	User::verifyLogin();
@@ -76,16 +77,40 @@ $app->post("/admin/categories/:idcategory", function ($idcategory){
 
 });
 
-$app->get("/categories/:idcategory", function($idcategory){
 
-$category = new Category();
-$category->get((int)$idcategory);
-$page = new Page();
-$page->setTpl("category", [
-	'category'=>$category->getValues(),
-	'products'=>[]
-]);
-
+$app->get("/admin/categories/:idcategory/products", function($idcategory){
+	User::verifyLogin();
+	$category = new Category();
+	$category->get((int)$idcategory);
+	$page = new PageAdmin();
+	$page->setTpl("categories-products", [
+		'category'=>$category->getValues(),
+		'productsRelated'=>$category->getProducts(),
+		'productsNotRelated'=>$category->getProducts(false)
+	]);
 
 });
+
+$app->get("/admin/categories/:idcategory/products/:idproduct/add", function($idcategory, $idproduct){
+	User::verifyLogin();
+	$category = new Category();
+	$category->get((int)$idcategory);
+	$product = new 	Product();
+	$product->get((int)$idproduct);
+	$category->addProduct($product);
+	header("Location: /admin/categories/".$idcategory."/products");
+	exit;
+});
+
+$app->get("/admin/categories/:idcategory/products/:idproduct/remove", function($idcategory, $idproduct){
+	User::verifyLogin();
+	$category = new Category();
+	$category->get((int)$idcategory);
+	$product = new Product();
+	$product->get((int)$idproduct);
+	$category->removeProduct($product);
+	header("Location: /admin/categories/".$idcategory."/products");
+	exit;
+});
+
 ?>
--- site.php
+++ site.php
@@ -1,6 +1,7 @@
 <?php
 use \Hcode\Page;
 use \Hcode\Model\Product;
+use \Hcode\Model\Category;
 
 $app->get('/', function() {
 
@@ -12,5 +13,16 @@ $app->get('/', function() {
 		'products'=>Product::checkList($products)
 	]);
 
+});
+
+$app->get("/categories/:idcategory", function($idcategory){
+
+$category = new Category();
+$category->get((int)$idcategory);
+$page = new Page();
+$page->setTpl("category", [
+	'category'=>$category->getValues(),
+	'products'=>Product::checkList($category->getProducts())
+]);
 });
 ?>
