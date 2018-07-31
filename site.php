7438129a0e8373eec62607ecf7248d9f8435d0d9
zidane steigleder <zidanesteigleder@gmail.com>
2018-07-31 15:38:37 -0300

    carrinho prodts

--- site.php
+++ site.php
@@ -53,8 +53,48 @@ $app->get("/products/:desurl", function($desurl){
 $app->get("/cart", function(){
 	$cart = Cart::getFromSession();
 	$page = new Page();
-	$page->setTpl("cart");
+	$page->setTpl("cart",[
+				'cart'=>$cart->getValues(),
+				'products'=>$cart->getProducts()
+	]);
 
 
-})
+});
+
+$app->get("/cart/:idproduct/add", function($idproduct){
+
+	$product = new Product();
+	$product->get((int)$idproduct);
+	$cart = Cart::getFromSession();
+	$qtd = (isset($_GET['qtd'])) ? (int)$_GET['qtd'] : 1;
+for ($i=0; $i < $qtd; $i++) {
+	  $cart->addProduct($product);
+}
+	header ("location: /cart");
+	exit;
+
+});
+
+$app->get("/cart/:idproduct/minus", function($idproduct){
+
+	$product = new Product();
+	$product->get((int)$idproduct);
+	$cart = Cart::getFromSession();
+	$cart-> removeProduct($product);
+	header ("location: /cart");
+	exit;
+
+});
+
+$app->get("/cart/:idproduct/remove", function($idproduct){
+
+	$product = new Product();
+	$product->get((int)$idproduct);
+	$cart = Cart::getFromSession();
+	$cart-> removeProduct($product, true);
+	header ("location: /cart");
+	exit;
+
+});
+
 ?>
