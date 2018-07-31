6d4ba6f1bb0cc6a12ea6580757435539fe5a281e
zidane steigleder <zidanesteigleder@gmail.com>
2018-07-31 14:49:30 -0300

    carrinho sessão

--- site.php
+++ site.php
@@ -2,6 +2,7 @@
 use \Hcode\Page;
 use \Hcode\Model\Product;
 use \Hcode\Model\Category;
+use \Hcode\Model\Cart;
 
 $app->get('/', function() {
 
@@ -48,4 +49,12 @@ $app->get("/products/:desurl", function($desurl){
 			]);
 
 });
+
+$app->get("/cart", function(){
+	$cart = Cart::getFromSession();
+	$page = new Page();
+	$page->setTpl("cart");
+
+
+})
 ?>
